acf520c27844e4af180d7fde92e1f46a7f63c51f
claconsay <claconsay@gmail.com>
2015-05-01 02:16:08 +0800

    modify plugin description. removed company and define requirements on plugin description

--- ipay88-gateway.php
+++ ipay88-gateway.php
@@ -1,23 +1,24 @@
 <?php
 /**
-Plugin Name: Events Manager - iPay88 Gateway
-Plugin URI: http://spurpress.com/
-Description: iPay88 Gateway for Event Manager. Looking for custom solutions? Please click <a href="http://www.spurpress.com/">here</a> to contact us.
-Version: 0.9
-Author: Christopher Laconsay
-Author URI: http://chrislaconsay.com
-*/
-
+  Plugin Name: Events Manager - iPay88 Gateway
+  Plugin URI: http://spurpress.com/
+  Description: iPay88 Gateway for Events Manager. This requires Events Manager Pro plugin for it to work.
+  Version: 0.9
+  Author: Christopher Laconsay
+  Author URI: http://chrislaconsay.com
+ */
 // If this file is called directly, abort.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH'))
+    exit;
 
 /**
  * events manager pro is a pre-requisite
  */
 function emp_ipay88_prereq() {
-    ?> <div class="error">
-	<p><?php _e('Events Manager - iPay88 Gateway needs <a href="http://eventsmanagerpro.com/">Events Manager Pro</a> installed and activated for it to work.','events-manager-ipay88'); ?></p>
-       </div>
+    ?> 
+    <div class="error">
+        <p><?php _e('Events Manager - iPay88 Gateway needs <a href="http://eventsmanagerpro.com/">Events Manager Pro</a> installed and activated for it to work.', 'events-manager-ipay88'); ?></p>
+    </div>
     <?php
 }
 
@@ -26,14 +27,14 @@ function emp_ipay88_prereq() {
  */
 function emp_ipay88_register() {
     //check that EM Pro is installed
-  if( ! defined( 'EMP_VERSION' ) ) {
-      add_action( 'admin_notices', 'emp_ipay88_prereq' );        
-   }
+    if (!defined('EMP_VERSION')) {
+        add_action('admin_notices', 'emp_ipay88_prereq');
+    }
 
     if (class_exists('EM_Gateways')) {
-		require_once( plugin_dir_path( __FILE__ ) . 'class-ipay88-gateway.php' );
-		EM_Gateways::register_gateway('ipay88', 'EM_Gateway_iPay88');
-	}
+        require_once( plugin_dir_path(__FILE__) . 'class-ipay88-gateway.php' );
+        EM_Gateways::register_gateway('ipay88', 'EM_Gateway_iPay88');
+    }
 }
 
-add_action( 'plugins_loaded', 'emp_ipay88_register', 1000);
+add_action('plugins_loaded', 'emp_ipay88_register', 1000);
