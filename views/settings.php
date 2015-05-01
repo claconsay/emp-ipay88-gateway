768ce41da09686c156d560a6689ac79799b0017b
claconsay <claconsay@gmail.com>
2015-05-01 19:16:48 +0800

    modify settings page according to iPay88 inputs

--- views/settings.php
+++ views/settings.php
@@ -1,3 +1,4 @@
+<?php 
 /*
 * iPay88 gateway add-on for Events Manager Pro
 * @package   Events_Manager_iPay88
@@ -5,27 +6,21 @@
 * @license GPL-2.0+ Christopher Laconsay
 * @copyright 2015 claconsay 
 */
-
-<?php 
-/*
- * TODO
- * - modify settings page according to iPay88
- */
 ?>
     <table class="form-table">
         <tbody>
             <tr valign="top">
-                <th scope="row"><?php _e('Test Mode', 'em-pro') ?></th>
+                <th scope="row"><?php _e('Gateway Status', 'em-pro') ?></th>
                 <td>	
-                    <select name="sentry_testmode">
+                    <select name="ipay88_status">
                         <?php
-                        $methods = array(
-                            'True' => 'True',
-                            'False' => 'False'
+                        $statuses = array(
+                            'live' => 'Live',
+                            'dev' => 'Development'
                         );
-                        $sentry_testmode = get_option('em_' . $this->gateway . '_testmode');
-                        foreach ($methods as $key => $value) {
-                            if ($sentry_testmode == $key) {
+                        $ipay88_status = $this->get_option('status');
+                        foreach ($statuses as $key => $value) {
+                            if ($ipay88_status == $key) {
                                 echo '<option value="' . $key . '" selected="selected">' . $value . '</option>';
                             } else {
                                 echo '<option value="' . $key . '">' . $value . '</option>';
@@ -33,31 +28,23 @@
                         }
                         ?>
                     </select>
-                    <br /><em><?php _e('Set to "True" if still on development.', 'em-pro'); ?></em>
+                    <br /><em><?php _e('Set this to Development if you are still testing the gateway.', 'em-pro'); ?></em>
                 </td>
             </tr>
             <tr valign="top">
                 <th scope="row"><?php _e('Success Message', 'em-pro') ?></th>
                 <td>
-                    <input type="text" name="sentry_booking_feedback" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_booking_feedback")); ?>" style='width: 40em;' /><br />
-                    <em><?php _e('The message that is shown to a user when a booking is successful whilst being redirected to Sentry for payment.', 'em-pro'); ?></em>
+                    <input type="text" name="ipay88_booking_feedback" value="<?php esc_attr_e($this->get_option("booking_feedback")); ?>" style='width: 40em;' /><br />
+                    <em><?php _e('The message that is shown to a user when a booking is successful whilst being redirected to iPay88 for payment.', 'em-pro'); ?></em>
                 </td>
             </tr>
             <tr valign="top">
                 <th scope="row"><?php _e('Success Free Message', 'em-pro') ?></th>
                 <td>
-                    <input type="text" name="sentry_booking_feedback_free" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_booking_feedback_free")); ?>" style='width: 40em;' /><br />
+                    <input type="text" name="ipay88_booking_feedback_free" value="<?php esc_attr_e($this->get_option("booking_feedback_free")); ?>" style='width: 40em;' /><br />
                     <em><?php _e('In some cases you offer a free ticket, this message will be shown and user won\'t be redirected to payment gateway.', 'em-pro'); ?></em>
                 </td>
             </tr>
-            <tr valign="top">
-                <th scope="row"><?php _e('Thank You Message', 'em-pro') ?></th>
-                <td>
-                    <input type="text" name="sentry_booking_feedback_thanks" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_booking_feedback_thanks")); ?>" style='width: 40em;' /><br />
-                    <em><?php _e('If you choose to return users to the default Events Manager thank you page after a user has paid on Sentry, you can customize the thank you message here.', 'em-pro'); ?></em>
-                </td>
-            </tr>
-
         </tbody>
     </table>
 
@@ -65,84 +52,51 @@
     <table class="form-table">
         <tbody>
             <tr valign="top">
-                <th scope="row"><?php _e('Version', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_version" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_version")); ?>" /><br /><em><?php _e('This is the version of the SENTRY Payment Gateway.', 'em-pro'); ?></em>
-                </td>
-            </tr>
-
-            <tr valign="top">
-                <th scope="row"><?php _e('Merchant ID', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_merchantid" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_merchantid")); ?>" /><br /><em><?php _e('(will be provided by Maybank)', 'em-pro'); ?></em>
+                <th scope="row"><?php _e('Merchant Code', 'em-pro') ?></th>
+                <td><input type="text" name="ipay88_mercode" value="<?php esc_attr_e($this->get_option("mercode")); ?>" /><br />
+                    <em><?php _e('Merchant code assigned by iPay88.', 'em-pro'); ?></em>
                 </td>
             </tr>
             <tr valign="top">
-                <th scope="row"><?php _e('Acquirer ID', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_acquirerid" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_acquirerid")); ?>" /><br /><em><?php _e('(will be provided by Maybank)', 'em-pro'); ?></em>
-                    <br />
-                </td>
-            </tr>	
-            <tr valign="top">
-                <th scope="row"><?php _e('Password', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_password" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_password")); ?>" /><br /><em><?php _e('This is the password included in the mailer that your provider supplied.', 'em-pro'); ?></em>
+                <th scope="row"><?php _e('Merchant Key', 'em-pro') ?></th>
+                <td><input type="text" name="ipay88_mercode" value="<?php esc_attr_e($this->get_option("mercode")); ?>" /><br />
+                    <em><?php _e('Merchant key assigned by iPay88.', 'em-pro'); ?></em>
                 </td>
             </tr>
             <tr valign="top">
-                <th scope="row"><?php _e('Currency', 'em-pro') ?></th>
-                <td><?php echo esc_html(get_option('dbem_bookings_currency', 'USD')); ?><br /><i><?php echo sprintf(__('Set your currency in the <a href="%s">settings</a> page.', 'dbem'), EM_ADMIN_URL . '&amp;page=events-manager-options'); ?></i></td>
+                <th scope="row"><?php _e('Payment ID', 'em-pro') ?></th>
+                <td><input type="text" name="ipay88_payid" value="<?php esc_attr_e($this->get_option("payid")); ?>" /><br />
+                    <em><?php _e('ID of payment method to be use. Default: 2', 'em-pro'); ?></em>
+                </td>
             </tr>
             <tr valign="top">
-                <th scope="row"><?php _e('Signature Method', 'em-pro') ?></th>
-                <td>	
-                    <select name="sentry_sigmethod">
-                        <?php
-                        $methods = array(
-                            'MD5' => 'MD5',
-                            'SHA1' => 'SHA-1'
-                        );
-                        $sentry_sigmethod = get_option('em_' . $this->gateway . '_sigmethod');
-                        foreach ($methods as $key => $value) {
-                            if ($sentry_sigmethod == $key) {
-                                echo '<option value="' . $key . '" selected="selected">' . $value . '</option>';
-                            } else {
-                                echo '<option value="' . $key . '">' . $value . '</option>';
-                            }
-                        }
-                        ?>
-                    </select>
-                    <br /><em><?php _e('Encryption method to be use.', 'em-pro'); ?></em>
-                </td>
+		<th scope="row"><?php _e('Currency', 'em-pro') ?></th>
+		<td><?php echo esc_html(get_option('dbem_bookings_currency','MYR')); ?><br />
+                    <em><?php echo sprintf(__('Set your currency in the <a href="%s">settings</a> page. Default: MYR','dbem'),EM_ADMIN_URL.'&amp;page=events-manager-options'); ?></em></td>
             </tr>
             <tr valign="top">
                 <th scope="row"><?php _e('Response URL', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_reponseurl" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_reponseurl")); ?>" style='width: 40em;' /><br /><em><?php _e('URL that will handle the response from Maybank. Must be in https. If unsure, please fill it in with the default value ', 'em-pro');
-                        echo '<strong>' . $this->get_payment_return_url() . '<strong>'; ?></em>
+                <td><input type="text" name="" value="<?php echo $this->get_payment_return_url(); ?>" disabled/><br />
+                    <em><?php _e('From the payment gateway page, transaction results will be sent to this URL and all the booking processes will be handled in this page.', 'em-pro');?></em>
                 </td>
             </tr>
             <tr valign="top">
                 <th scope="row"><?php _e('Thank You URL', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_thankyouurl" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_thankyouurl")); ?>" style='width: 40em;' /><br /><em><?php _e('If you have used the default Reponse URL, user will be redirected to this page if payment is succesful. Create a new page and add the link here.)', 'em-pro'); ?></em>
+                <td><input type="text" name="ipay88_thankyouurl" value="<?php esc_attr_e($this->get_option("thankyouurl")); ?>" style='width: 40em;' /><br />
+                    <em><?php _e('Client will be redirected to this page if the payment is approved. Create a new page and add the link here.)', 'em-pro'); ?></em>
                 </td>
             </tr>
             <tr valign="top">
                 <th scope="row"><?php _e('Payment Cancelled  URL', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_paycancelled" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_paycancelled")); ?>" style='width: 40em;' /><br /><em><?php _e('User will be redirected to this page when they cancel the payment.', 'em-pro'); ?></em>
-                </td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php _e('RedirectLink URL', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_redirectlink" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_redirectlink")); ?>" style='width: 40em;' /><br /><em><?php _e('URL where merchant data will be sent (will be provided by Maybank)', 'em-pro'); ?></em>
-                </td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php _e('RedirectLink URL: Test Mode', 'em-pro') ?></th>
-                <td><input type="text" name="sentry_redirectlink_test" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_redirectlink_test")); ?>" style='width: 40em;' /><br /><em><?php _e('Booking will be sent to this URL if set to test mode.', 'em-pro'); ?></em>
+                <td><input type="text" name="ipay88_paycancelled" value="<?php esc_attr_e($this->get_option("paycancelled")); ?>" style='width: 40em;' /><br />
+                    <em><?php _e('Client will be redirected to this page in case they cancel or their payment didn\'t go through. Create a new page and add the link here.', 'em-pro'); ?></em>
                 </td>
             </tr>
             <tr valign="top">
                 <th scope="row"><?php _e('Delete Bookings Pending Payment', 'em-pro') ?></th>
                 <td>
-                    <input type="text" name="sentry_booking_timeout" style="width:50px;" value="<?php esc_attr_e(get_option('em_' . $this->gateway . "_booking_timeout")); ?>" style='width: 40em;' /> <?php _e('minutes', 'em-pro'); ?><br />
-                    <em><?php _e('Once a booking is started and the user is taken to Sentry Payment Page, Events Manager stores a booking record in the database to identify the incoming payment. If you would like these bookings to expire after x minutes, please enter a value above.', 'em-pro'); ?></em>
+                    <input type="text" name="ipay88_booking_timeout" style="width:50px;" value="<?php esc_attr_e($this->get_option("booking_timeout")); ?>" style='width: 40em;' /> <?php _e('minutes', 'em-pro'); ?><br />
+                    <em><?php _e('Once a booking is started and the user is taken to iPay88 Payment Page, Events Manager stores a booking record in the database to identify the incoming payment. If you would like these bookings to expire after x minutes, please enter a value above.', 'em-pro'); ?></em>
                 </td>
             </tr>
         </tbody>
