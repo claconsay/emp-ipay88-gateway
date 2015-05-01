434a57563aa8c7ced0d25729820c938a79ece0c0
claconsay <claconsay@gmail.com>
2015-05-01 19:18:58 +0800

    added copyright on the header

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
+/*
+ * iPay88 gateway add-on for Events Manager Pro
+ * @package   Events_Manager_iPay88
+ * @author Christopher Laconsay <claconsay[at]gmail[dot]com>
+ * @license GPL-2.0+
+ * @copyright 2015 claconsay 
+ */
+
 // Silence is golden.
